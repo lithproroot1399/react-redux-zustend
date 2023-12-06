0bb1c0b088cc754c93d0216d55bc3ef1d09b2770
lithproroot1399 <prorootinfo@gmail.com>
2023-12-06 20:17:53 +0000

    concluindo aula

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -28,7 +28,7 @@ export function Player()  {
                 />
                 </div> 
             </div>
-                <aside className='w-80 absolute top-0 botton-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbala-thumb-800'>
+                <aside className='w-80 absolute top-0 botton-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbala-thumb-800'>
                     <div>
                         <button className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
                         <div className='flex h-10 w-10 rounded-full items-center justify-center bg-zinc-900 text-xs'>
@@ -40,27 +40,6 @@ export function Player()  {
                     </div>
                     <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
                     </button>
-                        <nav className='relative flex flex-col gap-4 p-6'>
-                            <button className='flex items-center gap-3 text-sm text-zinc-400'>
-                                <Video className='w-4 h-4 text-zinc-500' />
-                                <span>Fundamentos do Redux</span>
-                                <span className='ml-auto font-mono text-xs text-zinc-500'>09:13</span>
-                            </button>
-
-                            <button className='flex items-center gap-3 text-sm text-zinc-400'>
-                                <Video className='w-4 h-4 text-zinc-500' />
-                                <span>Fundamentos do Redux</span>
-                                <span className='ml-auto font-mono text-xs text-zinc-500'>09:13</span>
-                            </button>
-
-                            <button className='flex items-center gap-3 text-sm text-zinc-400'>
-                                <Video className='w-4 h-4 text-zinc-500' />
-                                <span>Fundamentos do Redux</span>
-                                <span className='ml-auto font-mono text-xs text-zinc-500'>09:13</span>
-                            </button>
-
-
-                        </nav>
                     </div>
                     <div>
                         <button className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
